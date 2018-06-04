6502eb42d69aa07e5b22a9c8b1bea7fbe4505855
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-06-04 10:07:39 +0200

    Add static data, display list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: "test 1", isComplete: true },
+      { id: 2, name: "test 2", isComplete: false },
+      { id: 3, name: "test 3", isComplete: false }
+    ],
+    currentTodo: ""
+  };
+
+  handleInputChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      currentTodo: event.target.value
+    });
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +28,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-App">
+          <form>
+            <input type="text" value={this.state.value} onChange={this.handleInputChange} />
+          </form>
+        </div>
+        <div className="Todo-list">
+          <ul>
+            {this.state.todos.map(todo => (
+              <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
