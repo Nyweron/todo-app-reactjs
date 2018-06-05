b2615dd9a3b403bbff1928ce38604209657734c4
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-06-05 09:17:36 +0200

    Add test to findbyid

--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import { addTodo } from "./todoHelpers";
+import { addTodo, findById } from "./todoHelpers";
 
 test("addTodo should add and passed todo to the list", () => {
   const startTodos = [
@@ -32,4 +32,16 @@ test("addTodo should not mutate the existing todo array", () => {
   const result = addTodo(startTodos, newTodo);
 
   expect(result).not.toBe(startTodos);
+});
+
+test("findById should find object by id from list", () => {
+  const startTodos = [
+    { id: 1, name: "one", isComplete: false },
+    { id: 2, name: "two", isComplete: false }
+  ];
+  const id = 1;
+  const expected = { id: 1, name: "one", isComplete: false };
+
+  const result = findById(startTodos, id);
+  expect(result).toEqual(expected);
 });
