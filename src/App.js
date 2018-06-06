3293446d0350cb81cecdcf4c756e48abded5a44a
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-06-06 10:09:27 +0200

    Add msg info to POST, PUT and DELETE methods

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { TodoForm, TodoList, TodoFilter } from "./components/todo/index";
-import { generateId, addTodo, findById, updateByObjectId, removeTodoById, filterTodos, getCurrentPath } from "./lib/todoHelpers";
+import {
+  generateId,
+  addTodo,
+  findById,
+  updateByObjectId,
+  removeTodoById,
+  filterTodos,
+  getCurrentPath
+} from "./lib/todoHelpers";
 import { getAll, createTodo, deleteTodo, updateTodo } from "./lib/todoService";
 
 class App extends Component {
   state = {
     todos: [],
     currentTodo: "",
-    errorMessage: "",
+
     currentRoute: ""
   };
 
   componentDidMount() {
-    getAll()
-      .then(todos => this.setState({ todos }))
+    getAll().then(todos => this.setState({ todos }));
   }
 
   handleInputChange = event => {
@@ -40,7 +47,7 @@ class App extends Component {
         errorMessage: ""
       });
 
-      createTodo(newTodo);
+      createTodo(newTodo).then(() => this.showTempMessage("Todo added"));
     }
   };
 
@@ -56,23 +63,32 @@ class App extends Component {
 
     const newUpdatedTodosList = updateByObjectId(listOfTodos, todo);
     this.setState({ todos: newUpdatedTodosList });
-    updateTodo(todo);
+    updateTodo(todo).then(() => this.showTempMessage("Todo updated"));
   };
 
   handleRemove = id => {
     let listOfTodos = this.state.todos;
     const newListWithoutRemovedItem = removeTodoById(listOfTodos, id);
     this.setState({ todos: newListWithoutRemovedItem });
-    deleteTodo(id);
+    deleteTodo(id).then(() => this.showTempMessage("Todo deleted"));
   };
 
   handleCurrentPath = () => {
     const currentPath = getCurrentPath();
-    this.setState({currentRoute: currentPath})
-  }
+    this.setState({ currentRoute: currentPath });
+  };
+
+  showTempMessage = msg => {
+    this.setState({ message: msg });
+    setTimeout(() => {
+      this.setState({ message: "" });
+    }, 2000);
+  };
 
   render() {
-    const submitHandle = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
+    const submitHandle = this.state.currentTodo
+      ? this.handleSubmit
+      : this.handleEmptySubmit;
     const SortTodos = filterTodos(this.state.todos, this.state.currentRoute);
     return (
       <div className="App">
@@ -84,13 +100,16 @@ class App extends Component {
           {this.state.errorMessage && (
             <span className="error">{this.state.errorMessage}</span>
           )}
+          {this.state.message && (
+            <span className="success">{this.state.message}</span>
+          )}
           <TodoForm
             handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
             handleSubmit={submitHandle}
           />
 
-          <TodoFilter handleCurrentPath={this.handleCurrentPath}/>
+          <TodoFilter handleCurrentPath={this.handleCurrentPath} />
 
           <TodoList
             handleToggle={this.handleToggle}
